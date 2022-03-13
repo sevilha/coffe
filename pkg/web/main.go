c9707615ca71aee1cf72c6c8e6ed24190283d8b3
Vinicius Sevilha <vd.sevilha@gmail.com>
2022-03-13 20:14:17 -0300

    CHG: add request log to server

--- pkg/web/main.go
+++ pkg/web/main.go
@@ -4,12 +4,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"sync/atomic"
 	"time"
 
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"github.com/sevilha/coffee/pkg/web/router"
 )
+
+type controller struct {
+	logger        *log.Logger
+	nextRequestID func() string
+	healthy       int64
+}
+
+type middleware func(http.Handler) http.Handler
+type middlewares []middleware
 
 func main() {
 	r := mux.NewRouter()
@@ -18,6 +29,11 @@ func main() {
 		negroni.NewLogger(),
 	)
 
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	logger.Printf("Server is starting...")
+
+	c := &controller{logger: logger, nextRequestID: func() string { return strconv.FormatInt(time.Now().UnixNano(), 36) }}
+
 	router.MakeCoffeeHandler(r, n)
 	http.Handle("/", r)
 
@@ -25,13 +41,47 @@ func main() {
 		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 30 * time.Second,
 		Addr:         ":4000",
-		Handler:      http.DefaultServeMux,
-		ErrorLog:     log.New(os.Stderr, "logger: ", log.Lshortfile),
+		Handler:      (middlewares{c.tracing, c.logging}).apply(r),
+		ErrorLog:     logger,
 	}
+
+	logger.Printf("Server is ready to handle requests at %q\n", ":4000")
+	atomic.StoreInt64(&c.healthy, time.Now().UnixNano())
 
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+func (c *controller) logging(hdlr http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		defer func(start time.Time) {
+			requestID := w.Header().Get("X-Request-Id")
+			if requestID == "" {
+				requestID = "unknown"
+			}
+			c.logger.Println(requestID, req.Method, req.URL.Path, req.RemoteAddr, time.Since(start))
+		}(time.Now())
+		hdlr.ServeHTTP(w, req)
+	})
+}
+
+func (c *controller) tracing(hdlr http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		requestID := req.Header.Get("X-Request-Id")
+		if requestID == "" {
+			requestID = c.nextRequestID()
+		}
+		w.Header().Set("X-Request-Id", requestID)
+		hdlr.ServeHTTP(w, req)
+	})
+}
+
+func (mws middlewares) apply(hdlr http.Handler) http.Handler {
+	if len(mws) == 0 {
+		return hdlr
+	}
+	return mws[1:].apply(mws[0](hdlr))
 }
